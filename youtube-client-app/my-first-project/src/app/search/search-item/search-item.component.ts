1f5eca156a32f2fd48fe0a1d3c2c82a81c15de1e
vikuli <vikukulichka.terekhova@gmail.com>
2022-10-08 22:15:50 +0300

    feat: add style for card container

--- youtube-client-app/my-first-project/src/app/search/search-item/search-item.component.ts
+++ youtube-client-app/my-first-project/src/app/search/search-item/search-item.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-search-item',
+  selector: '<app-search-item></app-search-item>',
   templateUrl: './search-item.component.html',
   styleUrls: ['./search-item.component.scss'],
 })
