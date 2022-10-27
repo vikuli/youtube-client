863e08a2cb608369997035d31bd57e41f5502f84
vikuli <vikukulichka.terekhova@gmail.com>
2022-10-27 16:02:57 +0300

    fix: fix application page paths

--- youtube-client-app/my-first-project/src/app/app-routing.module.ts
+++ youtube-client-app/my-first-project/src/app/app-routing.module.ts
@@ -1,17 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { ErrorComponent } from './youtube/error/error.component';
-import { SearchItemsComponent } from './youtube/search-item/search-item.component';
-import { AuthGuard } from './core/guards/auth.guard';
 
 const routes: Routes = [
   {
     path: '',
-    component: SearchItemsComponent,
-    canActivate: [AuthGuard],
-  },
-  {
-    path: 'videos',
     loadChildren: () =>
       import('./youtube/youtube.module').then((mod) => mod.YoutubeModule),
   },
@@ -19,12 +11,8 @@ const routes: Routes = [
     path: 'authorization',
     loadChildren: () =>
       import('./authorization/authorization.module').then(
-        (mod) => mod.AuthorizationModule,
+        (mod) => mod.AuthorizationModule
       ),
-  },
-  {
-    path: 'error',
-    component: ErrorComponent,
   },
   { path: '**', redirectTo: '/error' },
 ];
--- youtube-client-app/my-first-project/src/app/youtube/youtube.module.ts
+++ youtube-client-app/my-first-project/src/app/youtube/youtube.module.ts
@@ -4,22 +4,31 @@ import { VideoDescriptionComponent } from './video-description/video-description
 import { RouterModule, Routes } from '@angular/router';
 import { AuthGuard } from '../core/guards/auth.guard';
 import { CommonModule } from '@angular/common';
+import { AdminPageComponent } from './admin-page/admin-page.component';
+import { ErrorComponent } from './error/error.component';
 
 const routes: Routes = [
   {
-    path: '',
+    path: 'videos',
     component: SearchItemsComponent,
     canActivate: [AuthGuard],
   },
   {
-    path: ':id',
+    path: 'videos/:id',
     component: VideoDescriptionComponent,
     canActivate: [AuthGuard],
   },
+  { path: 'admin', component: AdminPageComponent, canActivate: [AuthGuard] },
+  { path: 'error', component: ErrorComponent, canActivate: [AuthGuard] },
 ];
 
 @NgModule({
-  declarations: [SearchItemsComponent, VideoDescriptionComponent],
+  declarations: [
+    SearchItemsComponent,
+    VideoDescriptionComponent,
+    AdminPageComponent,
+    ErrorComponent,
+  ],
   imports: [CommonModule, RouterModule.forChild(routes)],
   exports: [RouterModule],
 })
