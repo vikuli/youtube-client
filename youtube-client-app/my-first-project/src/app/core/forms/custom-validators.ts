34525dff5e82b59c48aadc91eae030c971aa99e3
vikuli <vikukulichka.terekhova@gmail.com>
2022-10-28 20:26:53 +0300

    feat: add validation rules for admin form

--- youtube-client-app/my-first-project/src/app/core/forms/custom-validators.ts
+++ youtube-client-app/my-first-project/src/app/core/forms/custom-validators.ts
@@ -32,4 +32,24 @@ export class CustomValidators {
     }
     return null;
   }
+
+  static urlValidity(control: FormControl): { [key: string]: boolean } | null {
+    const validExp =
+      /^(?:(?:https?|ftp|telnet):\/\/(?:[a-z0-9_-]{1,32}(?::[a-z0-9_-]{1,32})?@)?)?(?:(?:[a-z0-9-]{1,128}\.)+(?:com|net|org|mil|edu|arpa|ru|gov|biz|info|aero|inc|name|[a-z]{2})|(?!0)(?:(?!0[^.]|255)[0-9]{1,3}\.){3}(?!0|255)[0-9]{1,3})(?:\/[a-z0-9.,_@%&?+=\~\/-]*)?(?:#[^ \'\"&<>]*)?$/;
+    if (control.value && !validExp.test(control.value)) {
+      return { urlValidity: true };
+    }
+    return null;
+  }
+
+  static actualDate(control: FormControl): { [key: string]: boolean } | null {
+    const currentDate: Date = new Date();
+    const enteredDate: Date = new Date(control.value);
+    let dateDiff =
+      (currentDate.getTime() - enteredDate.getTime()) / 1000 / 60 / 60 / 24;
+    if (control.value && (isNaN(dateDiff) || dateDiff < 0)) {
+      return { actualDate: true };
+    }
+    return null;
+  }
 }
--- youtube-client-app/my-first-project/src/app/youtube/admin-page/admin-page.component.ts
+++ youtube-client-app/my-first-project/src/app/youtube/admin-page/admin-page.component.ts
@@ -1,17 +1,35 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { CustomValidators } from 'src/app/core/forms/custom-validators';
 
 @Component({
   selector: 'app-admin-page',
   templateUrl: './admin-page.component.html',
-  styleUrls: ['./admin-page.component.scss']
+  styleUrls: ['./admin-page.component.scss'],
 })
 export class AdminPageComponent implements OnInit {
   form!: FormGroup;
-  constructor() { }
 
   ngOnInit(): void {
-    this.form = new FormGroup({})
+    this.form = new FormGroup({
+      title: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3),
+        Validators.maxLength(20),
+      ]),
+      discription: new FormControl('', [Validators.maxLength(255)]),
+      imgLink: new FormControl('', [
+        Validators.required,
+        CustomValidators.urlValidity,
+      ]),
+      videoLink: new FormControl('', [
+        Validators.required,
+        CustomValidators.urlValidity,
+      ]),
+      date: new FormControl('', [
+        Validators.required,
+        CustomValidators.actualDate,
+      ]),
+    });
   }
-
 }
